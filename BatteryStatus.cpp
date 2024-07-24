6a3555adda7e1b4211a684557359965774c5d5a5
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-24 23:45:30 +0530

    Update BatteryStatus.cpp

--- BatteryStatus.cpp
+++ BatteryStatus.cpp
@@ -4,5 +4,5 @@
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
-  assert(batteryIsOK(-10, 23, 0.6) == false);
+  assert(batteryIsOk(-10, 23, 0.6) == false);
 }
