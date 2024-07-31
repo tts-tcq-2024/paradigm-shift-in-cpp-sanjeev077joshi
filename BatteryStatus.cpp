3e55288c979e748a5a909fc650b3205a996b9f93
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-31 20:55:10 +0530

    Update BatteryStatus.cpp

--- BatteryStatus.cpp
+++ BatteryStatus.cpp
@@ -1,8 +1,18 @@
 #include <assert.h>
+#include <iostream>
+#include <string>
 #include "CheckBatteryStatus.h"
 
+extern bool test_checkValueInRange();
+extern void tsst_CheckWarningForGivenValue();
+
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
-  assert(batteryIsOk(-10, 23, 0.6) == false);
+
+	test_checkValueInRange();
+	tsst_CheckWarningForGivenValue();
+
+	/* Test cases for InRange Validation */
+    assert(batteryIsOk(25, 70, 0.7) == true);
+    assert(batteryIsOk(50, 85, 0) == false);
+    assert(batteryIsOk(-10, 23, 0.6) == false);
 }
